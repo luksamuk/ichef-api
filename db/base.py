d26cdf0b1af3d8ea16a285e43ca6fb970f07128d
Lucas S. Vieira <lucasvieira@protonmail.com>
2024-01-02 20:30:49 -0300

    Make default timestamps non-nullable

--- db/base.py
+++ db/base.py
@@ -6,6 +6,6 @@ DeclarativeBase = declarative_base()
 
 class DatabaseModel(DeclarativeBase):
     __abstract__ = True
-    created_at = Column(DateTime, default=datetime.now, index=True)
-    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    created_at = Column(DateTime, default=datetime.now, index=True, nullable=False)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now, nullable=False)
 
