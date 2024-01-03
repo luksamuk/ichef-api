e643faeb7b574406f4095917395a52b6843cab2f
Lucas S. Vieira <lucasvieira@protonmail.com>
2024-01-03 00:41:52 -0300

    Fix ping route test

--- tests/test_ping.py
+++ tests/test_ping.py
@@ -4,7 +4,7 @@ from fastapi.testclient import TestClient
 client = TestClient(app)
 
 def test_ping():
-    response = client.get('/')
+    response = client.get('/ping')
     assert response.status_code ==  200
     assert response.json() ==  { 'message': 'pong' }
 
